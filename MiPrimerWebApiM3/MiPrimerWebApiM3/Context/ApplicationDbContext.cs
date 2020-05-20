7e2749313e6ed1a1cc99ce563544be67d1c74dae
David Robinson <david.robinson@logicstudio.net>
2020-05-20 15:45:52 -0500

    feat(primer-webapi): creacion del proyecto y configuraciones
    configuracion de EFCore

---
+++ MiPrimerWebApiM3/MiPrimerWebApiM3/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimerWebApiM3.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApiM3.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
