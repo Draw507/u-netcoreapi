9aba3a669b4b0fb3c902e3c4106f236812df813e
David Robinson <david.robinson@logicstudio.net>
2020-06-12 10:29:40 -0500

    feat(primer-webapi): validacion personalizada por modelo

--- MiPrimerWebApiM3/MiPrimerWebApiM3/Entities/Autor.cs
+++ MiPrimerWebApiM3/MiPrimerWebApiM3/Entities/Autor.cs
@@ -7,12 +7,25 @@ using System.Threading.Tasks;
 
 namespace MiPrimerWebApiM3.Entities
 {
-    public class Autor
+    public class Autor: IValidatableObject
     {
         public int Id { get; set; }
         [Required]
         [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public List<Libro> Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula", new string[] { nameof(Nombre) });
+                }
+            }
+        }
     }
 }
