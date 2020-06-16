16c9112df1094bb6ec71c8fd9641045352e586c3
David Robinson <draw507@hotmail.com>
2020-06-16 17:34:47 -0500

    feat(seguridad): configuracion JWT

---
+++ Seguridad/Seguridad/Models/Usertoken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seguridad.Models
+{
+    public class Usertoken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
