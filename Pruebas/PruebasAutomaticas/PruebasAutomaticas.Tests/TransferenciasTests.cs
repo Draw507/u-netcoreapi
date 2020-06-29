8d3cdde522f7bdc5069b254e81fd5890978a75ff
David Robinson <draw507@hotmail.com>
2020-06-29 10:43:01 -0500

    feat(pruebas): moq

--- Pruebas/PruebasAutomaticas/PruebasAutomaticas.Tests/TransferenciasTests.cs
+++ Pruebas/PruebasAutomaticas/PruebasAutomaticas.Tests/TransferenciasTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 
 namespace PruebasAutomaticas.Tests
 {
@@ -6,8 +8,32 @@ namespace PruebasAutomaticas.Tests
     public class TransferenciasTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TransferenciaInvalidaArrojaException()
         {
+            // Preparación
+            Exception expectedException = null;
+            Cuenta origen = new Cuenta() { Fondos = 0 };
+            Cuenta destino = new Cuenta() { Fondos = 0 };
+            decimal montoATransferir = 5m;
+            var mock = new Mock<IServicioValidacionesDeTransferencias>();
+            string mensajeDeError = "mensaje de error";
+            mock.Setup(x => x.RealizarValidaciones(origen, destino, montoATransferir)).Returns(mensajeDeError);
+            var servicio = new ServicioDeTransferencias(mock.Object);
+
+            // Prueba
+            try
+            {
+                servicio.TransferirEntreCuentas(origen, destino, montoATransferir);
+                Assert.Fail("Un error debió ser arrojado");
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+
+            // Verificación
+            Assert.IsTrue(expectedException is ApplicationException);
+            Assert.AreEqual(mensajeDeError, expectedException.Message);
         }
     }
 }
